7c319e7188e22fdbce3eef0d6be821d250b95dfe
Bogdan Rancichi <bogdan.rancichi@emag.ro>
2017-09-09 21:43:52 +0300

    [campaign] added entity and create endpoint

--- src/AppBundle/Model/Product.php
+++ src/AppBundle/Model/Product.php
@@ -23,4 +23,84 @@ class Product
      * @var int
      */
     protected $maxReach;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return Product
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return Product
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     *
+     * @return Product
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMaxReach()
+    {
+        return $this->maxReach;
+    }
+
+    /**
+     * @param int $maxReach
+     *
+     * @return Product
+     */
+    public function setMaxReach($maxReach)
+    {
+        $this->maxReach = $maxReach;
+
+        return $this;
+    }
 }
